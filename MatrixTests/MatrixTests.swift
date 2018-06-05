0b7772d5f62f1b648689ba67687669fc887b0130
Neil Alexander <neilalexander@users.noreply.github.com>
2018-06-05 10:09:41 +0100

    Initial Commit

---
+++ MatrixTests/MatrixTests.swift
@@ -0,0 +1,36 @@
+//
+//  MatrixTests.swift
+//  MatrixTests
+//
+//  Created by Neil Alexander on 05/06/2018.
+//  Copyright © 2018 Neil Alexander. All rights reserved.
+//
+
+import XCTest
+@testable import Matrix
+
+class MatrixTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
