77197e3abe060c71e2c191a910092a9fd7ebbd7b
Neil Alexander <neilalexander@users.noreply.github.com>
2018-08-06 11:52:26 +0100

    Show room history and access in room settings

--- Seaglass/Controller/MainViewRoomSettingsController.swift
+++ Seaglass/Controller/MainViewRoomSettingsController.swift
@@ -7,13 +7,37 @@
 //
 
 import Cocoa
+import SwiftMatrixSDK
 
 class MainViewRoomSettingsController: NSViewController {
     @IBOutlet var RoomName: NSTextField!
     @IBOutlet var RoomTopic: NSTextField!
     @IBOutlet var RoomPublishInDirectory: NSButton!
     
+    @IBOutlet var RoomAccessOnlyInvited: NSButton!
+    @IBOutlet var RoomAccessExceptGuests: NSButton!
+    @IBOutlet var RoomAccessIncludingGuests: NSButton!
+    
+    @IBOutlet var RoomHistorySinceJoined: NSButton!
+    @IBOutlet var RoomHistorySinceInvited: NSButton!
+    @IBOutlet var RoomHistorySinceSelected: NSButton!
+    @IBOutlet var RoomHistoryAnyone: NSButton!
+    
     public var roomId: String = ""
+    
+    @IBAction func accessRadioClicked(_ sender: NSButton) {
+        for radio in [ RoomAccessOnlyInvited, RoomAccessExceptGuests, RoomAccessIncludingGuests ] {
+            radio?.state = .off
+        }
+        sender.state = .on
+    }
+    
+    @IBAction func historyRadioClicked(_ sender: NSButton) {
+        for radio in [ RoomHistorySinceJoined, RoomHistorySinceInvited, RoomHistorySinceSelected, RoomHistoryAnyone ] {
+            radio?.state = .off
+        }
+        sender.state = .on
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,10 +47,12 @@ class MainViewRoomSettingsController: NSViewController {
             return
         }
         
-        RoomName.stringValue = MatrixServices.inst.session.room(withRoomId: roomId).state.name ?? ""
-        RoomTopic.stringValue = MatrixServices.inst.session.room(withRoomId: roomId).state.topic ?? ""
+        let room = MatrixServices.inst.session.room(withRoomId: roomId) as MXRoom
         
-        MatrixServices.inst.session.room(withRoomId: roomId).getDirectoryVisibility(completion: { (visibility) in
+        RoomName.stringValue = room.state.name ?? ""
+        RoomTopic.stringValue = room.state.topic ?? ""
+        
+        room.getDirectoryVisibility(completion: { (visibility) in
             if visibility.isSuccess {
                 self.RoomPublishInDirectory.isEnabled = true
                 self.RoomPublishInDirectory.state = visibility.value == .public ? .on : .off
@@ -34,6 +60,15 @@ class MainViewRoomSettingsController: NSViewController {
                 self.RoomPublishInDirectory.state = .off
             }
         })
+        
+        RoomAccessOnlyInvited.state = !room.state.isJoinRulePublic ? .on : .off
+        RoomAccessExceptGuests.state = room.state.isJoinRulePublic && room.state.guestAccess == .forbidden ? .on : .off
+        RoomAccessIncludingGuests.state = room.state.isJoinRulePublic && room.state.guestAccess == .canJoin ? .on : .off
+        
+        RoomHistorySinceJoined.state = room.state.historyVisibility == .invited ? .on : .off
+        RoomHistorySinceInvited.state = room.state.historyVisibility == .joined ? .on : .off
+        RoomHistorySinceSelected.state = room.state.historyVisibility == .shared ? .on : .off
+        RoomHistoryAnyone.state = room.state.historyVisibility == .worldReadable ? .on : .off
     }
     
 }
