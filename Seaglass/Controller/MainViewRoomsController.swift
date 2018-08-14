3cafb07d3db3acf3904c2b85b66535373cb4e183
Neil Alexander <neilalexander@users.noreply.github.com>
2018-08-14 17:44:55 +0100

    Revert "Add output for #21"
    This reverts commit 3ec320a014799b115aa583f2c42f1763b7fc1914.

--- Seaglass/Controller/MainViewRoomsController.swift
+++ Seaglass/Controller/MainViewRoomsController.swift
@@ -97,23 +97,13 @@ class MainViewRoomsController: NSViewController, MatrixRoomsDelegate, NSTableVie
     }
     
     func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
-        print("Render room cell \(row)")
         let cell = tableView.makeView(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: "RoomListEntry"), owner: self) as? RoomListEntry
         cell?.identifier = nil
         
-        let roomCache = roomsCacheController.arrangedObjects as! [RoomsCacheEntry]
-        if row >= roomCache.count {
-            print("Selected row \(row) exceeds room cache count \(roomCache.count)")
-            return cell
-        }
-        print("Room cache size OK")
-        let state: RoomsCacheEntry = roomCache[row]
-        print("Room state found")
+        let state: RoomsCacheEntry = (roomsCacheController.arrangedObjects as! [RoomsCacheEntry])[row]
         cell?.roomsCacheEntry = state
-        print("Room state loaded into cell")
     
         let count = state.members().count
-        print("Room member count found")
 
         if state.roomName != "" {
             cell?.RoomListEntryName.stringValue = state.roomName
@@ -167,27 +157,19 @@ class MainViewRoomsController: NSViewController, MatrixRoomsDelegate, NSTableVie
     }
     
     func tableViewSelectionDidChange(_ notification: Notification) {
-        let row = notification.object as? NSTableView
-        if row == nil {
-            print("Table select notification is empty")
+        let row = notification.object as! NSTableView
+        
+        if row.selectedRow < 0 || row.selectedRow >= (roomsCacheController.arrangedObjects as! [RoomsCacheEntry]).count {
             return
         }
-        if row!.selectedRow < 0 || row!.selectedRow >= (roomsCacheController.arrangedObjects as! [RoomsCacheEntry]).count {
-            print("Table select row invalid \(row!.selectedRow)")
-            return
-        }
-        let entry = row!.view(atColumn: 0, row: row!.selectedRow, makeIfNecessary: false) as? RoomListEntry
+        
+        let entry = row.view(atColumn: 0, row: row.selectedRow, makeIfNecessary: true) as? RoomListEntry
         if entry != nil {
-            print("Table select OK")
             entry!.RoomListEntryUnread.isHidden = true
             
             DispatchQueue.main.async {
-                print("Start dispatch")
                 self.mainController?.channelDelegate?.uiDidSelectRoom(entry: entry!)
-                print("Completed dispatch")
             }
-        } else {
-            print("Table select NOT OK")
         }
     }
 }
