25b85c8aaa21b63b1c5bd3dcad7b613ccfa7dfb2
Aaron Raimist <aaron@raim.ist>
2018-08-13 22:00:01 -0500

    Add .travis.yml

--- SeaglassTests/SeaglassTests.swift
+++ SeaglassTests/SeaglassTests.swift
@@ -17,20 +17,13 @@
 //
 
 import XCTest
+@testable import Seaglass
 
-class MatrixUITests: XCTestCase {
-        
+class SeaglassTests: XCTestCase {
+    
     override func setUp() {
         super.setUp()
-        
         // Put setup code here. This method is called before the invocation of each test method in the class.
-        
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
     
     override func tearDown() {
@@ -39,8 +32,15 @@ class MatrixUITests: XCTestCase {
     }
     
     func testExample() {
-        // Use recording to get started writing UI tests.
+        // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
     }
     
 }
