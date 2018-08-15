5cd6b890b2fce849834396e37d6192bcaa790642
Neil Alexander <neilalexander@users.noreply.github.com>
2018-08-15 23:17:50 +0100

    Improve avatar selection for small rooms with no set avatar (#26)

--- Seaglass/Controller/Main View/MainViewRoomsController.swift
+++ Seaglass/Controller/Main View/MainViewRoomsController.swift
@@ -150,10 +150,14 @@ class MainViewRoomsController: NSViewController, MatrixRoomsDelegate, NSTableVie
             cell?.RoomListEntryName.stringValue = ""
         }
         
-        if state.roomAvatar == "" && state.members().count <= 2 {
-            if state.members()[0].userId == MatrixServices.inst.session.myUser.userId {
-                cell?.RoomListEntryIcon.setAvatar(forUserId: state.members()[1].userId)
-            } else {
+        if state.roomAvatar == "" {
+            if state.members().count == 2 {
+                if state.members()[0].userId == MatrixServices.inst.session.myUser.userId {
+                    cell?.RoomListEntryIcon.setAvatar(forUserId: state.members()[1].userId)
+                } else {
+                    cell?.RoomListEntryIcon.setAvatar(forUserId: state.members()[0].userId)
+                }
+            } else if state.members().count == 1 {
                 cell?.RoomListEntryIcon.setAvatar(forUserId: state.members()[0].userId)
             }
         } else {
