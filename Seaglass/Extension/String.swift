5b46c5eb1f6e7e3b1c43ca9c8b78e475a3f89f64
Neil Alexander <neilalexander@users.noreply.github.com>
2018-08-18 11:38:25 +0100

    Make monospace text a bit more grey and a bit less black

--- Seaglass/Extension/String.swift
+++ Seaglass/Extension/String.swift
@@ -30,7 +30,7 @@ extension String {
         }
         
         let parser = TSMarkdownParser.standard()
-        parser.monospaceAttributes["NSColor"] = NSColor.black
+        parser.monospaceAttributes["NSColor"] = NSColor.init(calibratedRed: 0.53, green: 0.54, blue: 0.58, alpha: 1.00)
         parser.monospaceAttributes["NSFont"] = NSFont.init(name: "Menlo", size: NSFont.smallSystemFontSize)
         parser.quoteAttributes[0]["NSColor"] = NSColor.gray
         parser.quoteAttributes[0]["NSFont"] = NSFont.systemFont(ofSize: NSFont.smallSystemFontSize)
