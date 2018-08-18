a4a07ada907978a2804b0e4d27afb11e2afa453f
Doug <git@pixlwave.uk>
2018-08-18 13:04:04 +0100

    Fix conflict

--- Seaglass/Extension/String.swift
+++ Seaglass/Extension/String.swift
@@ -30,7 +30,6 @@ extension String {
         }
         
         let parser = TSMarkdownParser.standard()
-        
         parser.monospaceAttributes["NSColor"] = NSColor(calibratedRed: 0.53, green: 0.54, blue: 0.58, alpha: 1.00)
         parser.monospaceAttributes["NSFont"] = NSFont(name: "Menlo", size: NSFont.smallSystemFontSize)
         parser.quoteAttributes[0]["NSColor"] = NSColor.gray
