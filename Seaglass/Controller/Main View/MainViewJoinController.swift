068bd11f9789ded779a431b7083f6677a3e41fc5
Neil Alexander <neilalexander@users.noreply.github.com>
2018-08-19 21:16:31 +0100

    Allow starting direct chat (#35)

--- Seaglass/Controller/Main View/MainViewJoinController.swift
+++ Seaglass/Controller/Main View/MainViewJoinController.swift
@@ -17,6 +17,7 @@
 //
 
 import Cocoa
+import SwiftMatrixSDK
 
 class MainViewJoinController: NSViewController {
     @IBOutlet weak var CreateRoomButton: NSButton!
@@ -47,7 +48,7 @@ class MainViewJoinController: NSViewController {
             CreateRoomButton.animator().alphaValue = 0
             CreateRoomSpinner.alphaValue = 1
         }, completionHandler: {
-            MatrixServices.inst.session.createRoom(parameters: [:]) { (response) in
+            MatrixServices.inst.session.createRoom(name: nil, visibility: nil, alias: nil, topic: nil, preset: MXRoomPreset.publicChat, completion: { (response) in
                 if response.isFailure, let error = response.error {
                     let alert = NSAlert()
                     alert.messageText = "Failed to create room"
@@ -57,35 +58,65 @@ class MainViewJoinController: NSViewController {
                     alert.runModal()
                 }
                 sender.window?.contentViewController?.dismiss(sender)
-            }
+            })
         })
     }
 
     @IBAction func joinRoomButtonClicked(_ sender: NSButton) {
         let room = self.JoinRoomText.stringValue
-        if !(room.starts(with: "#") || room.starts(with: "!")) || !room.contains(":") {
+        if !(room.starts(with: "#") || room.starts(with: "!") || room.starts(with: "@")) || !room.contains(":") {
             return
         }
         for control in controls {
             control.isEnabled = false
         }
         JoinRoomSpinner.startAnimation(sender)
+        
         NSAnimationContext.runAnimationGroup({ (context) in
             context.duration = 0.5
-            JoinRoomButton.animator().alphaValue = 0 
+            JoinRoomButton.animator().alphaValue = 0
             JoinRoomSpinner.alphaValue = 1
         }, completionHandler: {
-            MatrixServices.inst.session.joinRoom(room, completion: { (response) in
-                if response.isFailure, let error = response.error {
-                    let alert = NSAlert()
-                    alert.messageText = "Failed to join room \(room)"
-                    alert.informativeText = error.localizedDescription
-                    alert.alertStyle = .warning
-                    alert.addButton(withTitle: "OK")
-                    alert.runModal()
-                }
-                sender.window?.contentViewController?.dismiss(sender)
-            })
+            if room.starts(with: "#") || room.starts(with: "!") {
+                MatrixServices.inst.session.joinRoom(room, completion: { (response) in
+                    if response.isFailure, let error = response.error {
+                        let alert = NSAlert()
+                        alert.messageText = "Failed to join room \(room)"
+                        alert.informativeText = error.localizedDescription
+                        alert.alertStyle = .warning
+                        alert.addButton(withTitle: "OK")
+                        alert.runModal()
+                    }
+                    sender.window?.contentViewController?.dismiss(sender)
+                })
+            } else if room.starts(with: "@") {
+                MatrixServices.inst.session.createRoom(name: nil, visibility: nil, alias: nil, topic: nil, preset: MXRoomPreset.trustedPrivateChat, completion: { (response) in
+                    if response.isFailure, let error = response.error {
+                        let alert = NSAlert()
+                        alert.messageText = "Failed to create room"
+                        alert.informativeText = error.localizedDescription
+                        alert.alertStyle = .warning
+                        alert.addButton(withTitle: "OK")
+                        alert.runModal()
+                        sender.window?.contentViewController?.dismiss(sender)
+                    } else {
+                        if let roomId = response.value?.roomId {
+                            let invitee = MXRoomInvitee.userId(room)
+                            MatrixServices.inst.session.room(withRoomId: roomId).invite(invitee, completion: { (response) in
+                                if response.isFailure, let error = response.error {
+                                    let alert = NSAlert()
+                                    alert.messageText = "Failed to invite \(room)"
+                                    alert.informativeText = error.localizedDescription
+                                    alert.alertStyle = .warning
+                                    alert.addButton(withTitle: "OK")
+                                    alert.runModal()
+                                }
+                                sender.window?.contentViewController?.dismiss(sender)
+                            })
+                        }
+                    }
+                })
+            }
         })
     }
 }
