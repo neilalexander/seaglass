5353074447a1289b4c6ac8b5bb594326c3527812
Neil Alexander <neilalexander@users.noreply.github.com>
2018-08-27 21:18:41 +0100

    Fix CFBundleVersion

--- scripts/set_build_number.sh
+++ scripts/set_build_number.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 version=$(sh scripts/version.sh)
+bundleversion=$(git rev-list HEAD --count 2>/dev/null)
 
 target_plist="$TARGET_BUILD_DIR/$INFOPLIST_PATH"
 dsym_plist="$DWARF_DSYM_FOLDER_PATH/$DWARF_DSYM_FILE_NAME/Contents/Info.plist"
 
 for plist in "$target_plist" "$dsym_plist"; do
   if [ -f "$plist" ]; then
-    /usr/libexec/PlistBuddy -c "Set :CFBundleVersion $version" "$plist"
+    /usr/libexec/PlistBuddy -c "Set :CFBundleVersion $bundleversion" "$plist"
     /usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString $version" "$plist"
   fi
 done
