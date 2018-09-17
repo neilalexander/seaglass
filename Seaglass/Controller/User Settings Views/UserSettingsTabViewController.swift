c2990dd25fa8da9f50f080a89f817d66c80aa606
Neil Alexander <neilalexander@users.noreply.github.com>
2018-09-17 14:27:12 +0100

    Groundwork for user settings window

---
+++ Seaglass/Controller/User Settings Views/UserSettingsTabViewController.swift
@@ -0,0 +1,30 @@
+//
+// Seaglass, a native macOS Matrix client
+// Copyright © 2018, Neil Alexander
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+import Cocoa
+
+class UserSettingsTabViewController: NSTabViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.tabView.tabViewType = .noTabsNoBorder
+        self.selectedTabViewItemIndex = 0
+    }
+    
+}
---
+++ Seaglass/Controller/User Settings Views/UserSettingsWindowController.swift
@@ -0,0 +1,36 @@
+//
+// Seaglass, a native macOS Matrix client
+// Copyright © 2018, Neil Alexander
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+import Cocoa
+
+class UserSettingsWindowController: NSWindowController, NSToolbarDelegate {
+
+    @IBOutlet weak var toolbar: NSToolbar!
+    
+    override func windowDidLoad() {
+        self.window!.toolbar = toolbar
+        
+        super.windowDidLoad()
+    }
+
+    @IBAction func didChangeTabs(_ sender: NSSegmentedControl) {
+        if let controller = self.window!.contentViewController as? UserSettingsTabViewController {
+            controller.tabView.selectTabViewItem(at: sender.selectedSegment)
+        }
+    }
+}
