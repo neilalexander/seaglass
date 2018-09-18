425239b0a85cd5732e69b48bed613cebd94febbe
Neil Alexander <neilalexander@users.noreply.github.com>
2018-09-19 00:01:58 +0100

    Make sure toolbar items get drawn

--- Seaglass/Controller/User Settings Views/UserSettingsWindowController.swift
+++ Seaglass/Controller/User Settings Views/UserSettingsWindowController.swift
@@ -20,15 +20,20 @@ import Cocoa
 
 class UserSettingsWindowController: NSWindowController, NSToolbarDelegate {
 
-    @IBOutlet weak var toolbar: NSToolbar!
-    
-    var defaultSize: NSRect?
-    
+    @IBOutlet var toolbar: NSToolbar!
+
     override func windowDidLoad() {
-        self.window!.toolbar = toolbar
-        defaultSize = self.window!.frame
-        
         super.windowDidLoad()
+        
+        if let window = self.window {
+            if toolbar.items.count == 0 {
+                toolbar.insertItem(withItemIdentifier: NSToolbarItem.Identifier.flexibleSpace, at: 0)
+                toolbar.insertItem(withItemIdentifier: NSToolbarItem.Identifier.init("SeaglassPreferences"), at: 1)
+                toolbar.insertItem(withItemIdentifier: NSToolbarItem.Identifier.flexibleSpace, at: 2)
+            }
+            
+            window.toolbar = toolbar
+        }
     }
 
     @IBAction func didChangeTabs(_ sender: NSSegmentedControl) {
