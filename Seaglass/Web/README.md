3ddea7a5834bd239966d19fd54af02786e749b45
Matthew Hodgson <matthew@matrix.org>
2019-04-07 23:17:08 +0200

    basic HTML sanitize, and add replaceEvent()

---
+++ Seaglass/Web/README.md
@@ -0,0 +1,4 @@
+To avoid depending on npm, we manually check in the needed deps.
+
+ * preact.min.js taken from https://cdn.jsdelivr.net/npm/preact/dist/preact.min.js
+ * sanitize-html.min.js taken from https://github.com/punkave/sanitize-html@1.20.0 and npm install && npm run minify
--- Seaglass/Web/RoomMessageView.js
+++ Seaglass/Web/RoomMessageView.js
@@ -3,15 +3,58 @@ const h = preact.h;
 // the array of events we want to show in the timeline, as a naughty global
 const timeline = [];
 
+// lifted directly from
+// https://github.com/matrix-org/matrix-react-sdk/blob/develop/src/HtmlUtils.js
+const PERMITTED_URL_SCHEMES = ['http', 'https', 'ftp', 'mailto', 'magnet'];
+
+const sanitizeHtmlParams = {
+    allowedTags: [
+        'font', // custom to matrix for IRC-style font coloring
+        'del', // for markdown
+        'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'blockquote', 'p', 'a', 'ul', 'ol', 'sup', 'sub',
+        'nl', 'li', 'b', 'i', 'u', 'strong', 'em', 'strike', 'code', 'hr', 'br', 'div',
+        'table', 'thead', 'caption', 'tbody', 'tr', 'th', 'td', 'pre', 'span', 'img',
+    ],
+    allowedAttributes: {
+        // custom ones first:
+        font: ['color', 'data-mx-bg-color', 'data-mx-color', 'style'], // custom to matrix
+        span: ['data-mx-bg-color', 'data-mx-color', 'style'], // custom to matrix
+        a: ['href', 'name', 'target', 'rel'], // remote target: custom to matrix
+        img: ['src', 'width', 'height', 'alt', 'title'],
+        ol: ['start'],
+        code: ['class'], // We don't actually allow all classes, we filter them in transformTags
+    },
+
+    // Lots of these won't come up by default because we don't allow them
+    selfClosing: ['img', 'br', 'hr', 'area', 'base', 'basefont', 'input', 'link', 'meta'],
+    
+    // URL schemes we permit
+    allowedSchemes: PERMITTED_URL_SCHEMES,
+    
+    allowProtocolRelative: false,
+    
+    // XXX: need to port over transformTags
+};
+
 class Message extends preact.Component {
     render() {
         const ev = this.props.event;
+        
+        let body;
+        if (ev.content.format === 'org.matrix.custom.html') {
+            const html = sanitizeHtml(ev.content.formatted_body, sanitizeHtmlParams);
+            body = h('div', { className: "body", dangerouslySetInnerHTML: { "__html" : html }});
+        }
+        else {
+            body = h('div', { className: "body" }, ev.content.body );
+        }
+        
         return (
             h('tr', { key: ev.eventId }, [
                 h('td', { className: "avatar" }, "[ :-) ]" ),
                 h('td', { className: "content" }, [
                     h('div', { className: "sender" }, ev.sender ),
-                    h('div', { className: "body" },   ev.content.formatted_body || ev.content.body ),
+                    body,
                 ]),
                 h('td', { className: "timestamp" }, "00:00" ),
             ])
@@ -71,17 +114,15 @@ window.onerror = function(error) {
     alert(error);
 };
 
-function removeEvents(events) {
-    // XXX: why would you ever need to remove a specific event?
-    // surely the common case is to replace all the events in the
-    // table with a new set of events, which can be done more quickly
-    // than going through deleting the old ones first...
-
+function replaceEvent(event) {
     for (const ev of events) {
-        const i = timeline.indexOf(ev.eventId);
-        timeline.splice(i, 1);
+        // TODO: rather than finding, we could try to track a eventId->index map
+        // but it's not obvious that the hassle of maintaining this map with
+        // accurate indexes is worth it
+        const i = timeline.findIndex(e => e.eventId === ev.eventId);
+        timeline[i] = ev;
     }
-
+    
     preact.render(h(Timeline, { timeline }), document.body, document.body.lastChild);
 }
 
